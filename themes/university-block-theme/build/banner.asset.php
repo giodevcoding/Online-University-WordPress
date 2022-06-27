c012d431c645a94e96d30b1f56dd12ef415ca2ae
Giovanni P <33172553+giodevcoding@users.noreply.github.com>
2022-06-27 16:31:22 -0700

    Delete themes/university-block-theme/build directory

--- themes/university-block-theme/build/banner.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element'), 'version' => 'e69094845532474f7c7a');
--- themes/university-block-theme/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'a1833372d001c3063e9b');
